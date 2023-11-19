909fe9fc73bacc5ea5c59132f053226c30037bf2
g_vochin <g_vochin@VALUEBOX3000>
2023-11-19 17:18:33 -0500

    Add project files.

---
+++ TodaySomethingWasCoded1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("This program will output how many characters a given string input contains, not including spaces.");
+Console.WriteLine("Please input a word or series of words: ");
+
+String ?userInput = Console.ReadLine();
+
+int userInputTotalCount = userInput.Count();
+
+int userInputSpaces = userInput.Count(Char.IsWhiteSpace);
+
+userInputTotalCount -= userInputSpaces;
+
+Console.WriteLine(userInputTotalCount.ToString());
